3ed93e13a433cf5f719d942fad43bec13b7af272
Alex Epelde <alex@epelde.net>
2024-12-31 16:18:01 +0100

    more firefox settings :)

--- modules/home-manager/programs/firefox.nix
+++ modules/home-manager/programs/firefox.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, config, ... }: {
 
-  options.myHome.firefox.enable = lib.mkEnableOption "firefox configuration";
+  options.myHome.firefox.enable = lib.mkEnableOption "firefox profile configuration";
 
   config.programs.firefox = lib.mkIf config.myHome.firefox.enable {
 
@@ -26,11 +26,22 @@
     profiles."${config.home.username}.default" = {
 
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        augmented-steam
+        betterttv
+        canvasblocker
         darkreader
+        decentraleyes
+        facebook-container
+        h264ify
         indie-wiki-buddy
         keepassxc-browser
+        privacy-badger
+        return-youtube-dislikes
         tampermonkey
+        to-google-translate
+        user-agent-string-switcher
         yomitan
+        youtube-shorts-block
         zotero-connector
       ];
 
@@ -43,7 +54,57 @@
         };
       };
 
-    };
+      settings = {
+        # Restore previous session.
+        "broswer.startup.page" = 3;
 
+        # Ask where to save downloaded files.
+        "browser.download.useDownloadDir" = false;
+
+        # Do not ask to save passwords.
+        "signon.rememberSignons" = false;
+
+        # Privacy settings.
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.globalprivacycontrol.enabled" = true;
+
+        # Pinned sites.
+        "browser.newtabpage.pinned" = [
+          {
+            title = "youtube";
+            url = "https://youtube.com";
+          }
+          {
+            title = "wanikani";
+            url = "https://wanikani.com";
+          }
+          {
+            title = "patchouli";
+            url = "https://patchoulihq.cc";
+          }
+          {
+            title = "nextcloud";
+            url = "https://cloud.patchoulihq.cc";
+          }
+          {
+            title = "arxiv";
+            url = "https://arxiv.org/list/math.GT/recent";
+          }
+          {
+            title = "ae433";
+            url = "https://ae433.user.srcf.net/about-to-get-very-silly";
+          }
+          {
+            title = "archwiki";
+            url = "https://wiki.archlinux.org";
+          }
+          {
+            title = "patreon";
+            url = "https://patreon.com";
+          }
+        ];
+      };
+    };
   };
 }
