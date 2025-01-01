fa1984ad552523fe275912910b2c0631d512a3b4
Alex Epelde <alex@epelde.net>
2025-01-01 15:50:30 +0100

    hy3 appearance 2

--- modules/home-manager/wayland/hyprland/hy3.nix
+++ modules/home-manager/wayland/hyprland/hy3.nix
@@ -8,10 +8,19 @@
 
     # Tab group settings.
     tabs = {
-      height = 30;
-      padding = 0;
+      height = 24;
+      padding = 6;
+      rounding = 6;
       text_height = 12;
-      text_padding = 6;
+      text_padding = 12;
+
+      "col.active" = "0xff81a2be";
+      "col.urgent" = "0xffcc6666";
+      "col.inactive" = "0xff282a2e";
+
+      "col.text.active" = "0xff282a2e";
+      "col.text.urgent" = "0xff282a2e";
+      "col.text.inactive" = "0xffc5c8c6";
     };
 
   };
