38af3c39a4e208b7960bfa7292de97c86ff7162a
Alex Epelde <alex@epelde.net>
2025-01-01 17:03:53 +0100

    nvidia vaapi

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -4,13 +4,24 @@
 
   config = {
 
-    # Install Nvidia kernel modules.
-    hardware.graphics.enable = true;
+    hardware.graphics = {
+      # Enable hardware-accelerated graphics.
+      enable = true;
+
+      # 32-bit support, e.g. for Wine.
+      enable32Bit=true;
+
+      # Additional graphics drivers.
+      extraPackages = with pkgs; [ nvidia-vaapi-driver ];
+    };
+
+    # Use Nvidia drivers in X11.
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
       # Use open source Nvidia drivers.
       open = true;
+
       # Enable kernel modesetting.
       modesetting.enable = true;
     };
