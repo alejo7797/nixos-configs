838fd245635efeb25ca83ab32d776e635728c23e
Alex Epelde <alex@epelde.net>
2025-01-01 12:20:21 +0100

    electron wayland

--- modules/home-manager/wayland/hyprland/default.nix
+++ modules/home-manager/wayland/hyprland/default.nix
@@ -43,6 +43,7 @@
         env = [
           "XCURSOR_SIZE, 24"
           "HYPRCURSOR_SIZE, 24"
+          "ELECTRON_OZONE_PLATFORM_HINT, auto"
           "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
           "_JAVA_AWT_WM_NONREPARENTING, 1"
         ];
