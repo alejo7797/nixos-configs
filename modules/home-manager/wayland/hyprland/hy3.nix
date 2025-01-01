5f91c2c357e476c33880ab0bffdf17a60c540923
Alex Epelde <alex@epelde.net>
2025-01-01 15:45:07 +0100

    hy3 appearance

--- modules/home-manager/wayland/hyprland/hy3.nix
+++ modules/home-manager/wayland/hyprland/hy3.nix
@@ -6,5 +6,13 @@
     # Create a tab group for the first window in a workspace.
     tab_first_window = true;
 
+    # Tab group settings.
+    tabs = {
+      height = 30;
+      padding = 0;
+      text_height = 12;
+      text_padding = 6;
+    };
+
   };
 }
