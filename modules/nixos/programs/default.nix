0eb6f6616e7855c7be71a637f50a069723d49f5c
Alex Epelde <alex@epelde.net>
2025-01-02 01:31:50 +0100

    thunderbird extensions

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -5,4 +5,13 @@
     ./fcitx5.nix ./vim.nix
   ];
 
+  # Install Thunderbird extensions.
+  programs.thunderbird.policies = {
+    Extensions = {
+      "Install" = [
+        "https://addons.thunderbird.net/thunderbird/downloads/latest/darkreader/latest.xpi"
+      ];
+    };
+  };
+
 }
