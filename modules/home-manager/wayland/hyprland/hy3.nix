2761ed09464c132a3d00f8243062b0c621f05d34
Alex Epelde <alex@epelde.net>
2025-01-10 19:11:18 +0100

    neovim keymaps

--- modules/home-manager/wayland/hyprland/hy3.nix
+++ modules/home-manager/wayland/hyprland/hy3.nix
@@ -3,13 +3,10 @@ let
   stylix-colors = config.lib.stylix.colors;
 in
 {
-  # Configure hy3.
   wayland.windowManager.hyprland.settings.plugin.hy3 = {
 
-    # Create a tab group for the first window in a workspace.
     tab_first_window = true;
 
-    # Tab group settings.
     tabs = {
       height = 30;
       padding = 0;
