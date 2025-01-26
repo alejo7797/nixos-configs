5fd20a43870223b055ce869f223e405d2989a90f
Alex Epelde <alex@epelde.net>
2025-01-26 15:54:01 -0500

    hardware

--- hosts/patchouli/hardware-configuration.nix
+++ hosts/patchouli/hardware-configuration.nix
@@ -1,1 +1,15 @@
-{ }
+{ config, lib, pkgs, modulesPath, ... }: {
+
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "uas" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface.
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
