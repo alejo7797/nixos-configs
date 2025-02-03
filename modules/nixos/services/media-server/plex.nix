b7532dff32ae9db67d9d7ebda4879067ddb6baa7
Alex Epelde <alex@epelde.net>
2025-02-03 15:41:20 -0500

    plex

--- modules/nixos/services/media-server/plex.nix
+++ modules/nixos/services/media-server/plex.nix
@@ -13,7 +13,42 @@ in
 
   config = lib.mkIf cfg.enable {
 
+    services = {
 
+      plex = {
+        enable = true;
+        group = "media";
+      };
 
+      # Run Plex behind our Nginx web server.
+      nginx.virtualHosts."plex.patchoulihq.cc" = {
+
+        # Generate an SSL certificate.
+        enableACME = true; forceSSL = true;
+
+        extraConfig = ''
+
+          # More compression.
+          gzip_proxied any;
+          gzip_disable "msie6";
+
+          # Buffering off.
+          proxy_buffering off;
+
+          # Headers for the WebSocket connection.
+          proxy_set_header Sec-WebSocket-Extensions $http_sec_websocket_extensions;
+          proxy_set_header Sec-WebSocket-Key $http_sec_websocket_key;
+          proxy_set_header Sec-WebSocket-Version $http_sec_websocket_version;
+
+        '';
+
+        locations."/" = {
+          # Proxy to Plex running on localhost.
+          proxyPass = "https://localhost:32400";
+          proxyWebsockets = true;
+        };
+
+      };
+    };
   };
 }
