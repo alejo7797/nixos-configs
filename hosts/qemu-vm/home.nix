4273b6a24e3566135a03761989aaf4d3cc499022
Alex Epelde <alex@epelde.net>
2025-02-17 11:44:17 -0500

    maybe we'll test things in a vm later

--- hosts/qemu-vm/home.nix
+++ hosts/qemu-vm/home.nix
@@ -1,14 +1,7 @@
-{ pkgs, lib, config, ... }: {
-
-  # Basic settings needed by Home Manager.
-  home.username = "ewan";
-  home.homeDirectory = "/home/ewan";
-  home.stateVersion = "24.11";
-
-  # Load up our custom theme.
-  myHome.style.enable = true;
-
-  # Set up sway, the i3-compatible Wayland compositor.
-  myHome.sway.enable = true;
-
+{
+  home = {
+    username = "ewan";
+    homeDirectory = "/home/ewan";
+    stateVersion = "24.11";
+  };
 }
