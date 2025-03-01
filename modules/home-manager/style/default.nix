3bdab798b9d9afe61102ddd9705ade1989103149
Alex Epelde <alex@epelde.net>
2025-03-01 11:01:44 -0500

    bye bye kvantum

--- modules/home-manager/style/default.nix
+++ modules/home-manager/style/default.nix
@@ -1,7 +1,7 @@
 {
-  pkgs,
-  config,
   lib,
+  config,
+  pkgs,
   ...
 }:
 
@@ -14,13 +14,15 @@ in
 
   options.myHome.style.enable = lib.mkEnableOption "user-level theming";
 
-  config = lib.mkIf cfg.enable ({
+  config = lib.mkIf cfg.enable {
 
     stylix.enable = true;
 
     xdg = {
       configFile = {
-        # Fix the look of KDE applications.
+        # Fix the look of QT applications.
+        "qt5ct/qt5ct.conf".source = ./qt5ct.conf;
+        "qt6ct/qt6ct.conf".source = ./qt6ct.conf;
         "kdeglobals".source = ./kdeglobals;
 
         "fontconfig" = {
@@ -47,65 +49,5 @@ in
       enable = true; package = pkgs.papirus-icon-theme;
       dark = "Papirus-Dark"; light = "Papirus-Light";
     };
-  }
-
-  //
-
-  {
-    # The following QT options are set by Stylix,
-    # but unfortunately not in the 24.11 release.
-
-    qt = {
-      enable = true;
-      # Welcome back, kvantum!
-      platformTheme.name = "qtct";
-      style.name = "kvantum";
-    };
-
-    xdg.configFile =
-
-      let
-
-        kvantumPackage =
-          let
-
-            kvconfig = config.lib.stylix.colors {
-              template = ./kvconfig.mustache;
-              extension = ".kvconfig";
-            };
-
-            svg = config.lib.stylix.colors {
-              template = ./kvantum-svg.mustache;
-              extension = "svg";
-            };
-
-          in
-
-            pkgs.runCommandLocal "base16-kvantum" { } ''
-              directory="$out/share/Kvantum/Base16Kvantum"
-              mkdir --parents "$directory"
-              cp ${kvconfig} "$directory/Base16Kvantum.kvconfig"
-              cp ${svg} "$directory/Base16Kvantum.svg"
-            '';
-
-        qtctConf = ''
-          [Appearance]
-          style="kvantum"
-          icon_theme="Papirus-Dark"
-        '';
-
-      in
-
-      {
-        "Kvantum/kvantum.kvconfig".source =
-          (pkgs.formats.ini { }).generate "kvantum.kvconfig"
-            { General.theme = "Base16Kvantum"; };
-
-        "Kvantum/Base16Kvantum".source =
-          "${kvantumPackage}/share/Kvantum/Base16Kvantum";
-
-        "qt5ct/qt5ct.conf".text = qtctConf;
-        "qt6ct/qt6ct.conf".text = qtctConf;
-      };
-  });
+  };
 }
