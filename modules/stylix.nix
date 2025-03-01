d181f0f00977392dc595cd8f3744427339483218
Alex Epelde <alex@epelde.net>
2025-03-01 10:47:34 -0500

    qtct stopgap

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,33 +1,22 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }: {
 
-{
   stylix = {
 
-    # Specifying an image is mandatory.
     image = builtins.fetchurl {
+      # Must specify a wallpaper image even if we don't really need it.
       url = "https://w.wallhaven.cc/full/zy/wallhaven-zye9ry.jpg";
       sha256 = "16d5pch4544knygndsslmh682fxp6sqwn5b9vnrb35ji7m5zfcm0";
     };
 
-    # Set the colorscheme.
+    # Tomorrow Night is still the best for me even now. Thank you chriskempson.
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
 
-    # Specify that we are using a dark colorscheme.
     polarity = "dark";
 
-    # Configure our desired fonts.
-    fonts = {
+    fonts = rec {
       sizes = {
         applications = 10;
         terminal = 11;
-      };
-
-      serif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Serif";
       };
 
       sansSerif = {
@@ -35,22 +24,25 @@
         name = "Noto Sans";
       };
 
+      # Why would I not?
+      serif = sansSerif;
+
       monospace = {
+        name = "Hack Nerd Font"; # Avoid grabbing all Nerd Fonts.
         package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
-        name = "Hack Nerd Font";
       };
 
       emoji = {
+        # Would prefer monochrome.
         package = pkgs.noto-fonts-emoji;
         name = "Noto Color Emoji";
       };
     };
 
-    # Use the default KDE cursor theme.
     cursor = {
+      # Use the default KDE cursor theme.
       package = pkgs.kdePackages.breeze;
-      name = "breeze_cursors";
-      size = 24;
+      name = "breeze_cursors"; size = 24;
     };
   };
 }
