099783f511497759de59b63991bd006b134343e7
Alex Epelde <alex@epelde.net>
2025-03-03 10:33:10 -0500

    nvm

--- modules/home-manager/wayland/default.nix
+++ modules/home-manager/wayland/default.nix
@@ -56,8 +56,7 @@ in
     programs.wofi = {
       enable = true;
       settings = {
-        mode = "drun,run"; # Run commands in system $PATH.
-        drun-print_command = true; run-print_command = true;
+        mode = "drun,run"; drun-print_command = true;
         width = "36%"; height = "40%"; allow_images = true;
         location = "center"; key_expand = "Ctrl-x";
       };
