81b4d27bdd522835f0693e9fb13c538658f61469
Alex Epelde <alex@epelde.net>
2025-03-12 12:06:18 -0400

    zsh dotdir fix

--- modules/nixos/programs/zsh.nix
+++ modules/nixos/programs/zsh.nix
@@ -5,7 +5,7 @@
     zsh = {
       enable = true;
       shellInit = ''
-        export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+        export ZDOTDIR="$HOME/.config/zsh"
       '';
       autosuggestions.enable = true;
       syntaxHighlighting.enable = true;
