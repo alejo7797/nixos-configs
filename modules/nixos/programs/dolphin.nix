238be9c6d2bc53f5126e551e2fc7ca43a15e0aa9
Alex Epelde <alex@epelde.net>
2025-03-12 10:13:19 -0400

    qt6ct-kde fix

--- modules/nixos/programs/dolphin.nix
+++ modules/nixos/programs/dolphin.nix
@@ -38,5 +38,62 @@ in
         "/xdg/menus/sway-applications.menu".source = plasma-applications;
       };
 
+    nixpkgs.overlays = [
+      (final: prev: with final; {
+        libsForQt5 = prev.libsForQt5.overrideScope (
+          qt5-final: qt5-prev: with qt5-final; {
+            qt5ct = qt5-prev.qt5ct.overrideAttrs (oldAttrs: {
+              buildInputs = oldAttrs.buildInputs ++
+                [
+                  qtquickcontrols2 kconfig
+                  kconfigwidgets kiconthemes
+                ];
+
+              nativeBuildInputs =  [
+                cmake wrapQtAppsHook qttools
+              ];
+
+              patches = [
+                (fetchurl {
+                  url = "https://raw.githubusercontent.com/ilya-fedin/nur-repository/refs/heads/master/pkgs/qt5ct/qt5ct-shenanigans.patch";
+                  hash = "sha256-fziJn5xcSdtqwf69p36god0342n5zSHdJScjRw/IbgY=";
+                })
+              ];
+
+              cmakeFlags = [
+                "-DPLUGINDIR=${placeholder "out"}/${qtbase.qtPluginPrefix}"
+              ];
+            });
+          });
+
+        qt6Packages = prev.qt6Packages.overrideScope (
+          qt6-final: qt6-prev: with kdePackages; with qt6-final; {
+            qt6ct = qt6-prev.qt6ct.overrideAttrs (oldAttrs: {
+              src = fetchFromGitHub {
+                owner = "ilya-fedin";
+                repo = "qt6ct";
+                tag = oldAttrs.version;
+                hash = "sha256-ePY+BEpEcAq11+pUMjQ4XG358x3bXFQWwI1UAi+KmLo=";
+              };
+
+              buildInputs = oldAttrs.buildInputs ++
+                [
+                  qtdeclarative kconfig
+                  kcolorscheme kiconthemes
+                ];
+
+              nativeBuildInputs =  [
+                cmake wrapQtAppsHook qttools
+              ];
+
+              cmakeFlags = [
+                "-DPLUGINDIR=${placeholder "out"}/${qtbase.qtPluginPrefix}"
+              ];
+            });
+          }
+        );
+      })
+    ];
+
   };
 }
