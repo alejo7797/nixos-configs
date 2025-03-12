8b7c0c45cfee8ab35fa722b57af4ddee2e09ce0d
Alex Epelde <alex@epelde.net>
2025-03-11 22:02:41 -0400

    steam home

--- modules/nixos/programs/steam.nix
+++ modules/nixos/programs/steam.nix
@@ -2,6 +2,10 @@
   nixpkgs.overlays = [
     (_: prev: {
       steam = prev.steam.override {
+        extraPreBwrapCmds = ''
+          mkdir -p $XDG_DATA_HOME/Steam/home
+        '';
+
         extraBwrapArgs = [
           "--bind $XDG_DATA_HOME/Steam/home $HOME"
           "--bind $XDG_DATA_HOME $HOME/.local/share"
