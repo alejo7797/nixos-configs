9bca6743967fb469d913affc4dae21c41a24fe8e
Alex Epelde <alex@epelde.net>
2025-03-15 19:39:07 -0400

    careful groups

--- users/ewan/default.nix
+++ users/ewan/default.nix
@@ -1,11 +1,18 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (config.users) groups;
+  inherit (groups) lp networkmanager scanner wheel;
+in
+
+{
 
   users.users.ewan = {
     shell = pkgs.zsh;
     hashedPasswordFile = config.sops.secrets.my-password.path;
-    extraGroups = [ "wheel" ]
-      ++ lib.optional config.networking.networkmanager.enable "networkmanager"
-      ++ lib.optionals config.services.saned.enable [ "scanner" "lp" ];
+    extraGroups = [ wheel.name ]
+      ++ lib.optional config.networking.networkmanager.enable networkmanager.name
+      ++ lib.optionals config.services.saned.enable [ scanner.name lp.name ];
     isNormalUser = true; linger = true;
   };
 
