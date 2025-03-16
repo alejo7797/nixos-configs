11971d7e4139dfb60424db5e72c5c7875d88e6bf
Alex Epelde <alex@epelde.net>
2025-03-16 14:50:48 -0400

    zsh too

--- modules/nixos/programs/zsh.nix
+++ modules/nixos/programs/zsh.nix
@@ -1,15 +1,16 @@
-{
-  programs = {
-    zsh = {
-      enable = true;
-      histFile = "$XDG_STATE_HOME/zsh/history";
-      shellInit = ''
-        # Keep Zsh config out of their $HOME directory.
-        export ZDOTDIR="$$XDG_CONFIG_HOME/zsh"
-      '';
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-    };
+{ config, ... }: {
+
+  programs.zsh = {
+    histFile = "$XDG_STATE_HOME/zsh/history";
+    shellInit = ''
+      # Keep Zsh config out of their $HOME directory.
+      export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+    '';
+    interactiveShellInit = ''
+      mkdir -p "$(dirname "${config.programs.zsh.histFile}")"
+    '';
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
   };
 
 }
