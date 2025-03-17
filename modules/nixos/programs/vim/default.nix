6c473d86e7d9e7b4b7cb59eb7d52b6481d776f48
Alex Epelde <alex@epelde.net>
2025-03-17 17:58:03 -0400

    vim doc

--- modules/nixos/programs/vim/default.nix
+++ modules/nixos/programs/vim/default.nix
@@ -2,21 +2,24 @@
 
   programs.vim = {
 
+    # Set $EDITOR to vim.
     defaultEditor = true;
 
+    # Include some useful plugins.
     package = pkgs.vim.customize {
 
       vimrcConfig = {
 
         packages.my.start = with pkgs.vimPlugins; [
 
-          base16-vim # Cute colorscheme collection.
-          vim-airline # Lean & mean Vim status bar.
-          vim-airline-themes # And its themes repo.
-          vim-oscyank # Universal clipboard plugin.
+          base16-vim # Colorscheme collection.
+          vim-airline # Lightweight status bar.
+          vim-airline-themes # And its themes.
+          vim-oscyank # Clipboard solution.
 
         ];
 
+        # Basic vimrc file we can use anywhere.
         customRC = builtins.readFile ./vimrc;
 
       };
