8bad3dd9264d59b9d82af75d8260db439a95fe5c
Alex Epelde <alex@epelde.net>
2025-03-21 13:58:56 -0400

    workspaces...

--- hosts/satsuki/home.nix
+++ hosts/satsuki/home.nix
@@ -35,7 +35,8 @@
 
     workspaces = {
       # TODO: look into this.
-      "DP-1" = [ "r[1-20]" ];
+      "DP-1" = [ 1 2 3 4 5 6 7 8 9 10
+        11 12 13 14 15 16 17 18 19 20 ];
       "eDP-1" = [ "name:extra" ];
     };
 
