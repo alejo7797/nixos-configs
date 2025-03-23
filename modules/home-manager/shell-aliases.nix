a13c36857fce112d28e3df1e3adc050bf9833524
Alex Epelde <alex@epelde.net>
2025-03-23 01:04:38 -0400

    airvpn

--- modules/home-manager/shell-aliases.nix
+++ modules/home-manager/shell-aliases.nix
@@ -1,12 +1,13 @@
 {
-  home.shellAliases = {
+  home.shellAliases = rec {
     # Handy aliases for building NixOS configurations from my flake.
     nixos-switch = "sudo nixos-rebuild switch --flake ~/Git/nixos-configs";
     nixos-build = "nixos-rebuild build --flake ~/Git/nixos-configs";
 
-    # Manage connection to my VPN server.
-    vpnup = "nmcli c up Koakuma_VPN";
-    vpndown = "nmcli c down Koakuma_VPN";
+    # Manage connection to AirVPN.
+    airvpn = "nmcli -t c show | grep -E wg0$ | cut -d : -f 1";
+    vpndown = "export airvpn=$(${airvpn}) && nmcli c down $airvpn";
+    vpnup = "nmcli c up $airvpn";
 
     # Make dmesg output more readable and friendly.
     dmesg = "sudo dmesg --human --reltime --color=always | less";
