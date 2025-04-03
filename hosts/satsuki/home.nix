5903f5eb40d76a691581c04b82bb9acd1ba40a04
Alex Epelde <alex@epelde.net>
2025-04-03 08:19:15 -0400

    hyprland: persistent workspace

--- hosts/satsuki/home.nix
+++ hosts/satsuki/home.nix
@@ -39,7 +39,7 @@
       # TODO: look into this.
       "DP-1" = [ 1 2 3 4 5 6 7 8 9 10
         11 12 13 14 15 16 17 18 19 20 ];
-      "eDP-1" = [ "name:extra" ];
+      "eDP-1, persistent:true" = [ "name:extra" ];
     };
   };
 
