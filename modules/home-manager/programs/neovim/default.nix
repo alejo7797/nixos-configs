605f5e474e1d2c728c6d89165132cb8b3334ce0f
Alex Epelde <alex@epelde.net>
2025-04-27 10:06:40 -0400

    more snippets

--- modules/home-manager/programs/neovim/default.nix
+++ modules/home-manager/programs/neovim/default.nix
@@ -103,7 +103,7 @@ in
           '';
           post = ''
             npairs.add_rules({
-              rule(" = {", "};", "nix"),
+              rule(" = ", ";", "nix"),
             })
           '';
         };
