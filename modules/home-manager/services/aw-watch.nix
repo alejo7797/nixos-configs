8781d06c5b04df6794a4dffcb6d175d00aa2d2fe
Alex Epelde <alex@epelde.net>
2025-05-02 18:15:35 -0400

    awatcher config

--- modules/home-manager/services/aw-watch.nix
+++ modules/home-manager/services/aw-watch.nix
@@ -1,16 +1,62 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
 
+let
+  inherit (config.xdg) configHome;
+in
+
+{
   services.activitywatch = {
 
     settings = {
-
+      # The default server configuration.
+      address = "127.0.0.1"; port = 5600;
     };
 
     watchers = {
 
-      aw-awatcher = {
+      # For improved Wayland support.
+      awatcher = { config, ... }: {
+
         package = pkgs.awatcher;
-        executable = "awatcher";
+
+        extraOptions = [
+          "--config" # By default, awatcher looks for the configuration elsewhere.
+          "${configHome}/activitywatch/${config.name}/${config.settingsFilename}"
+        ];
+
+        settings = {
+
+          awatcher.filters = [
+            {
+              match-app-id = "kitty"; # Report a more friendly window title for toggleterm.
+              match-title = "zsh;#toggleterm#\\d+ - \\(term:\\/\\/(.*)\\/\\/\\d+:.*\\) - NVIM";
+              replace-title = "toggleterm.nvim - $1 - NVIM"; replace-app-id = "neovim";
+            }
+            {
+              match-app-id = "kitty"; # Keep window title stable.
+              match-title = "(.*[^+]) (?:\\+ )?(\\(.*\\) - NVIM)";
+              replace-app-id = "neovim"; replace-title = "$1 $2";
+            }
+            {
+              match-app-id = "org\\.kde\\.dolphin";
+              replace-app-id = "dolphin"; # Shorten.
+            }
+            {
+              match-app-id = "org\\.keepassxc\\.KeePassXC";
+              replace-app-id = "keepassxc"; # Just the same.
+            }
+            {
+              match-app-id = "org\\.pwmt\\.zathura";
+              replace-app-id = "zathura"; # Again.
+            }
+            {
+              match-app-id = "com\\.github\\.iwalton3\\.jellyfin-media-player";
+              replace-app-id = "jellyfin-media-player"; # And another app id.
+            }
+          ];
+
+        };
+
       };
 
     };
