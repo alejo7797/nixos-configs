d3eace569455d2d1f01b0d5e1fae04ef6109838e
Alex Epelde <alex@epelde.net>
2025-05-06 07:26:02 -0400

    comment flake

--- flake.nix
+++ flake.nix
@@ -16,52 +16,55 @@
     };
 
     nix-index-database = {
-      # Quickly locate packages giving specfic files.
+      # Helpful add-on to the nix-index location tool.
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nur = {
-      # Access to the Nix User Repository.
+      # Get access to Nix User Repository.
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     lanzaboote = {
-      # Secure Boot support for NixOS systems.
+      # Enable Secure Boot support for NixOS systems.
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     my-expressions = {
-      # My personal scripts, derivations and Nixpkgs overlays.
+      # My personal scripts, Nix derivations and Nixpkgs overlays.
       url = "gitlab:alex/nix-expressions?host=git.patchoulihq.cc";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      # Neovim configuration system built on Nix.
+      # Manage your Neovim config and LSPs using Nix.
       url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     autofirma-nix = {
-      # Suite of tools for working with Spain's administration.
+      # Toolset to interact with Spain's public administration.
       url = "github:nix-community/autofirma-nix/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-mailserver = {
-      # NixOS module setting up a complete and simple 10/10 email server.
+      # NixOS module setting up a complete but simple 10/10 email server.
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix/release-24.11";
-    ilya-fedin.url = "github:ilya-fedin/nur-repository";
-    impermanence.url = "github:nix-community/impermanence";
+    # Set up an manage a modded Minecraf server on NixOS.
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+
+    # Secret management tool for NixOS using sops.
     sops-nix.url = "github:Mic92/sops-nix/master";
+
+    # A theming framework for NixOS and Home Manager.
+    stylix.url = "github:danth/stylix/release-24.11";
 
   };
 
