17fc8acceb66574dd5904c29212aa076a1d039de
Alex Epelde <alex@epelde.net>
2025-05-07 09:57:00 -0400

    kitty: let zsh set title

--- modules/home-manager/programs/kitty/default.nix
+++ modules/home-manager/programs/kitty/default.nix
@@ -9,27 +9,17 @@ in
 
     programs.kitty = {
 
+      shellIntegration.mode = "no-title";
+
       settings = {
         # Set desired font appearance for dark background.
-        "font_family" = config.stylix.fonts.monospace.name;
-        "text_composition_strategy" = "2.0 0";
+        font_family = config.stylix.fonts.monospace.name;
+        text_composition_strategy = "2.0 0";
 
-        "enable_audio_bell" = "yes";
-        "linux_bell_theme" = "ocean";
+        enable_audio_bell = "yes";
+        linux_bell_theme = "ocean";
       };
 
-      extraConfig = ''
-        include theme.conf
-      '';
-
-    };
-
-    # We set the color theme ourselves.
-    stylix.targets.kitty.enable = false;
-
-    xdg.configFile = {
-      # TODO: figure out how to patch properly.
-      "kitty/theme.conf".source = ./theme.conf;
     };
 
   };
