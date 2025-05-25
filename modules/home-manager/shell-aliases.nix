ac9c62ae5a1ec9945589a287408ed7f690b250ff
Alex Epelde <alex@epelde.net>
2025-05-25 10:17:54 +0100

    nixos-switch aliases

--- modules/home-manager/shell-aliases.nix
+++ modules/home-manager/shell-aliases.nix
@@ -1,10 +1,29 @@
+{ lib, ... }:
+
+let
+  rebuild-args = lib.concatStringsSep " " [
+
+    # Some basic build output logging.
+    "--print-build-logs" "--verbose"
+
+    # Path to my personal NixOS configs.
+    "--flake ~/Git\\ Repos/nixos-configs"
+
+    # Used by nix-output-monitor.
+    "--log-format internal-json"
+
+  ];
+in
+
 {
   home.shellAliases = {
+
     # Handy aliases for building NixOS configurations from my flake.
-    nixos-switch = "sudo nixos-rebuild switch --flake ~/Git/nixos-configs";
-    nixos-build = "nixos-rebuild build --flake ~/Git/nixos-configs";
+    nixos-switch = "sudo nixos-rebuild switch ${rebuild-args} |& nom --json";
+    nixos-build = "nixos-rebuild build ${rebuild-args} |& nom --json";
 
     # Make dmesg output more readable, in color, and use a pager.
     dmesg = "sudo dmesg --human --reltime --color=always | less";
+
   };
 }
