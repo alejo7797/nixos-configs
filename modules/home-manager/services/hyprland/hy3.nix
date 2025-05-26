4e9ca91a8781e4461dc9df23bc7dbef166271f3c
Alex Epelde <alex@epelde.net>
2025-05-26 21:30:07 +0100

    hy3 borders

--- modules/home-manager/services/hyprland/hy3.nix
+++ modules/home-manager/services/hyprland/hy3.nix
@@ -16,15 +16,15 @@ in
       text_font = "Noto Sans Medium";
 
       "col.active" = "0xff${stylix-colors.base00}";
-      "col.active.border" = "0xff${stylix-colors.base0D}";
+      "col.active.border" = "0xff${stylix-colors.base00}";
       "col.active.text" = "0xff${stylix-colors.base05}";
 
       "col.inactive" = "0xff${stylix-colors.base00}";
-      "col.inactive.border" = "0xff${stylix-colors.base03}";
+      "col.inactive.border" = "0xff${stylix-colors.base00}";
       "col.inactive.text" = "0xff${stylix-colors.base03}";
 
       "col.focused" = "0xff${stylix-colors.base00}";
-      "col.focused.border" = "0xff${stylix-colors.base03}";
+      "col.focused.border" = "0xff${stylix-colors.base00}";
       "col.focused.text" = "0xff${stylix-colors.base03}";
 
       "col.urgent" = "0xff${stylix-colors.base08}";
