1c24c70c4f00feafeb4a29108dbe2e3037343024
Alex Epelde <alex@epelde.net>
2025-05-26 18:02:22 +0100

    hy3 update

--- modules/home-manager/services/hyprland/hy3.nix
+++ modules/home-manager/services/hyprland/hy3.nix
@@ -6,21 +6,31 @@ in
   wayland.windowManager.hyprland.settings.plugin.hy3 = {
 
     tabs = {
+
       height = 32;
       padding = 0;
-      rounding = 0;
+      radius = 0;
+
       text_height = 12;
-      text_padding = 12;
 
       text_font = "Noto Sans Medium";
 
       "col.active" = "0xff${stylix-colors.base00}";
-      "col.urgent" = "0xff${stylix-colors.base08}";
-      "col.inactive" = "0xff${stylix-colors.base00}";
+      "col.active.border" = "0xff${stylix-colors.base0D}";
+      "col.active.text" = "0xff${stylix-colors.base05}";
 
-      "col.text.active" = "0xff${stylix-colors.base05}";
-      "col.text.urgent" = "0xff${stylix-colors.base00}";
-      "col.text.inactive" = "0xff${stylix-colors.base03}";
+      "col.inactive" = "0xff${stylix-colors.base00}";
+      "col.inactive.border" = "0xff${stylix-colors.base03}";
+      "col.inactive.text" = "0xff${stylix-colors.base03}";
+
+      "col.focused" = "0xff${stylix-colors.base00}";
+      "col.focused.border" = "0xff${stylix-colors.base03}";
+      "col.focused.text" = "0xff${stylix-colors.base03}";
+
+      "col.urgent" = "0xff${stylix-colors.base08}";
+      "col.urgent.border" = "0xff${stylix-colors.base08}";
+      "col.urgent.text" = "0xff${stylix-colors.base00}";
+
     };
   };
 }
